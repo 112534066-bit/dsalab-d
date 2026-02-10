3bfdd9688cd729d644187768107268f38baa008a
112534066-bit <112534066@kanchiuniv.ac.in>
2026-02-10 02:57:20 -0800

    Add files via upload

---
+++ binarysearch.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int binarysearch(int arr[],int n,int key)
+{
+	int low = 0 ,high = n-1,mid;
+	while(low<=high)
+	{
+		mid = (low + high)/2;
+		if(arr[mid] == key)
+		return mid;    //element found
+		else if (arr[mid]<key)
+		low = mid + 1;   //search right half
+		else
+		high = mid-1;  //search left half
+	}
+	return -1;   //element not found
+}
+int main()
+{
+	int i,n,key,result;
+	printf("enter number of elements");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("enter %d sorted elements;\n ",n);
+	for (i = 0;i<n;i++)
+	scanf("%d",&arr[i]);
+	
+	printf("enter element to search:");
+	scanf("%d",&key);
+	result = binarysearch(arr,n,key);
+	
+	if (result!=-1)
+	printf("elements found at index%d\n",result);
+	else
+	printf("element not found\n");
+	return 0;
+}
+
